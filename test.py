fceb3b6d7c63b239a0434d20b8d60c24ce09e3ea
Toshiki <to4king@gmail.com>
2019-07-10 14:58:20 +0900

    add test function in test.py
    引数型一致しない場合の例外発生用テストを追加

--- test.py
+++ test.py
@@ -7,14 +7,28 @@ class TestPyCsvUtil:
         """ 範囲区切り文字が文字列型であることのテスト """
         csv_util = PyCsvUtil()
         assert isinstance(csv_util._range_delimiter, str)
+    
+    def test_list_to_str_arg_type(self):
+        """ 配列→文字列変換メソッドの引数テスト """
+        csv_util = PyCsvUtil()
+        not_list_arg = ''
+        with pytest.raises(TypeError):
+            csv_util.list_to_str(not_list_arg)
 
-    def test_list_to_str(self):
+    def test_list_to_str_return_type(self):
         """ 配列→文字列変換メソッドの戻り値テスト """
         csv_util = PyCsvUtil()
         test_list = [1, 2, 3]
         assert isinstance(csv_util.list_to_str(test_list), str)
 
     def test_str_to_list_arg_type(self):
+        """ 文字列→配列変換メソッドの引数テスト """
+        csv_util = PyCsvUtil()
+        not_str_arg = []
+        with pytest.raises(TypeError):
+            csv_util.str_to_list(not_str_arg)
+
+    def test_str_to_list_return_type(self):
         """ 文字列→配列変換メソッドのテスト """
         csv_util = PyCsvUtil()
         str_arg = '1,2,6-9,16'
