9c3acecdcfbda30177c3ccbab35b51c78af47312
toshi <to4king@gmail.com>
2021-09-21 03:11:24 +0900

    refactor comment

--- test.py
+++ test.py
@@ -18,30 +18,29 @@ class TestPyCsvUtil:
         """ 引数のデリミタの型チェック """
         with pytest.raises(TypeError):
             csv_util = PyCsvUtil(1)
-
     
     def test_list_to_str_arg_type(self):
-        """ 配列→文字列変換メソッドの引数テスト """
+        """ 配列→文字列変換メソッドの引数型チェック """
         csv_util = PyCsvUtil()
         not_list_arg = ''
         with pytest.raises(TypeError):
             csv_util.list_to_str(not_list_arg)
 
     def test_list_to_str_return_type(self):
-        """ 配列→文字列変換メソッドの戻り値テスト """
+        """ 配列→文字列変換メソッドの戻り値strであること """
         csv_util = PyCsvUtil()
         test_list = [1, 2, 3]
         assert isinstance(csv_util.list_to_str(test_list), str)
 
     def test_str_to_list_arg_type(self):
-        """ 文字列→配列変換メソッドの引数テスト """
+        """ 文字列→配列変換メソッドの引数型チェック """
         csv_util = PyCsvUtil()
         not_str_arg = []
         with pytest.raises(TypeError):
             csv_util.str_to_list(not_str_arg)
 
     def test_str_to_list_return_type(self):
-        """ 文字列→配列変換メソッドのテスト """
+        """ 文字列→配列変換メソッドの戻り値listであること """
         csv_util = PyCsvUtil()
         str_arg = '1,2,6-9,16'
         assert isinstance(csv_util.str_to_list(str_arg), list)
